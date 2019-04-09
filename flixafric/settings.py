ee4e09924f9696a3f96b46a5023e89e3fa2fa5b1
hakimks <hakimks@gmail.com>
2019-04-09 18:50:08 +0300

    feat: Added several things

--- flixafric/settings.py
+++ flixafric/settings.py
@@ -15,6 +15,8 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+ROOT_DIR = os.path.abspath(os.path.join(BASE_DIR, os.pardir))
+
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
 
@@ -143,4 +145,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+STATIC_ROOT = os.path.join(ROOT_DIR, 'static')
--- movies/models.py
+++ movies/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.model import User
 
+from django.utils import timezone
+
 # Create your models here.
 
 class Movie(models.Model):
@@ -9,13 +11,47 @@ class Movie(models.Model):
     poster = models.ImageField(upload_to="posters" default="poster/default.jpg")
     mainbackdrop = models.ImageField(upload_to="backdrop")
     otherbackdrop = models.ImageField(upload_to="backdrop")
-    director = models.CharField(max_length=150)
-    created_by = models.ForeignKey(User)
+    director = models.ForeignKey(Director, on_delete=models.CASCADE)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
     created_date =  models.DateField(default=timezone.now)
     slug = models.SlugField(max_length=150)
+    genre = models.ForeignKey(Genre, on_delete=modes.CASCADE)
+    actors = models.ManyToManyField(Actor, through='MovieActor')
+    productioncompany = models.ForeignKey(Company, on_delete=models.CASCADE)
+    length = models.IntergerField()
 
     def __str__(self):
         return self.title
 
     def Meta:
         ordering = ["created_date"]
+
+class Actor(models.Model):
+    name = models.CharField(max_length=200)
+    photo = models.ImageField(upload_to="photos/actor" default="photos/actot/default.jpg")
+
+    def __str__(sef):
+        return self.names
+
+class Genre(models.Model):
+    """ Holds the Genres like Action, Thriller """
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.name
+
+class Director(models.Model):
+    """ Holds the Name of the movie Director """
+    name = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.name
+
+class Company(models.Model):
+    """ Holds the Name of the production Company """
+    name = models.CharField(max_length=150)
+    photo = models.ImageField(upload_to="photos/productco" default="photos/productco/default.jpg")
+
+    def __str__(self):
+        return self.name
